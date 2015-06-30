00446e6437cf575c241f5e4d83829de02e062cc1
Daniel Smith <dan@shrapnel3d.com>
2015-06-30 22:36:02 +1200

    update

--- src/main/java/com/corprall/practicemod/main.java
+++ src/main/java/com/corprall/practicemod/main.java
@@ -1,5 +1,22 @@
 package com.corprall.practicemod;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="PracticeMod", name="Practice Mod", version="1.7.10-1.0")
 public class main {
 
+    @Mod.Instance("PracticeMod")
+    public static main instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){}
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){}
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){}
 }
